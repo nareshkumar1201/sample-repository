db5aeb8c6ffc9a568c609993551eb7a5f989a002
naresh <naresh.akaram@gmail.com>
2019-05-18 23:02:47 +0530

    js file edited

--- git.js
+++ git.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+$(document).ready(function(){
+    $('#btn1').on('mousedown',function(){
+         $('#p1').toggle();
+         $('h1').append('appended heading');
+         $('h1').addClass("headClass");
+         alert('mousedown event triggered');
+     });
+
+     $('#btn2').hover(function(){
+         $('#p2').toggle();
+         alert('mouseenter event');
+     },
+     function(){
+       $('#p2').toggle();
+
+      alert('mouseleave event');
+     });
+   /*  $('p').on('dblclick',function(e){
+       
+         $(e.currentTarget).toggle();
+
+     });*/
+ });
